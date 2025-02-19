6164438d0306dbe63a69b50c122e95670ec68586
Steve Scott <sscott1@cityops.nyc.gov>
2025-02-19 15:45:56 -0500

    done

--- README.md
+++ README.md
@@ -2,3 +2,22 @@
 ### Geometry To Topo JSON Converter ###
 ###        By Steve Scott           ###
 #######################################
+
+# converting a geospatial file to a topojson. #
+## Why convert to a TopoJSON? ##
+Most geojson files are coordinate based. A topoJSON uses a series of arcs to store its geometry. Why use TopoJSON? Because PowerBI's Shape Map visulaization component only works with TopoJSON files.
+
+## requirements ###
+```bash
+pip install geopandas==1.0.1
+pip install topojson
+pip install geojson
+pip install pytest
+```
+To use:
+
+```powershell
+python \Path\To\repo\convert_to_topo.py \path\of\file\to\convert\<filename>.geojson
+```
+
+This will generate a file named <filename>_topojson.json
---
+++ geo_to_topo.py
@@ -0,0 +1,40 @@
+
+import geopandas as gpd
+import topojson as tp
+import geojson
+import argparse
+from pathlib import Path
+
+def main(input_file : str, precision : float, string_cols : list[str]):
+    if string_cols is not None:
+        gdf = gpd.read_file(input_file, dtypes = {col: 'str' for col in string_cols if string_cols is not None})
+    else:
+        gdf = gpd.read_file(input_file)
+    if precision != 0:
+        gdf['geometry'] = gdf.simplify(0.0001)
+    else :
+        gdf['geometry'] = gdf.geometry
+
+    gdf.info()
+    this_json = gdf.to_json()
+    topo = tp.Topology(this_json)
+    
+    path = Path(input_file)
+    filename = path.stem
+    directory = path.parent
+    save_path = directory / (filename + '.topojson.json')
+    topo.to_json(str(save_path), 'wgs84')
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Convert geography to TopoJSON')
+    parser.add_argument('input_geo_file', type=str, help='Input file path to geography. It can be a geojson or a shapefile, or any other geopandas compatable file.')
+    #add the optional precision argument
+    parser.add_argument('-p', '--precision', type=float, default=0.0001, help='''
+                        Simplification precision of the TopoJSON output in decimal degrees. 
+                        The default is 0.0001 degrees which is around 8 to 11 meters. Enter 0 to disable simplification''')
+    parser.add_argument('-s', "--string_cols", nargs='+', help='List of columns to convert to string')
+    args = parser.parse_args()
+    input_file = args.input_geo_file
+    precision = args.precision
+    string_cols = args.string_cols
+    main(input_file, precision, string_cols)
